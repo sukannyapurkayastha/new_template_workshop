6285e93857d85fa916905016012329135a0ee5e6
sukannyapurkayastha <sukannyapurkayastha@users.noreply.github.com>
2024-03-07 10:08:15 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="akatief"
-original_name="template_workshop"
-original_urlname="template-workshop"
-original_description="Awesome template_workshop created by akatief"
+original_author="sukannyapurkayastha"
+original_name="new_template_workshop"
+original_urlname="new_template_workshop"
+original_description="Awesome new_template_workshop created by sukannyapurkayastha"
 # Iterate over all files in the repository
 git ls-files | while read -r filename; do
     # Exclude .github/workflows/rename_project.yml from renaming
---
+++ new_template_workshop/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for new_template_workshop."""
+
+from new_template_workshop.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
